3e304eb006a124a15fe8a37101d08fc02434392a
RGB Studios <justingolden21@gmail.com>
2018-10-08 01:28:32 -0700

    misc updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Run Calc
 
+<img src="img/icon.svg" width="48px">
+
 https://justingolden21.github.io/run-calc/
 
 A simple mile time calculator
--- scripts.js
+++ scripts.js
@@ -4,12 +4,22 @@ function calc() {
 	let miles = parseFloat($('#mi').val() );
 
 	if(miles==0 || isNaN(miles) ) {
-		$('#result').html("Miles cannot be zero");
+		$('#result').html('Miles cannot be zero');
+		$('#result').removeClass('bg-info');
+		$('#result').removeClass('bg-success');
+		$('#result').addClass('bg-warning');
 		return;
 	} else if(seconds==0 || isNaN(seconds) ) {
-		$('#result').html("Time cannot be zero");
+		$('#result').html('Time cannot be zero');
+		$('#result').removeClass('bg-info');
+		$('#result').removeClass('bg-success');
+		$('#result').addClass('bg-warning');
 		return;
 	}
+
+	$('#result').removeClass('bg-warning');
+	$('#result').removeClass('bg-info');
+	$('#result').addClass('bg-success');
 		
 	let secPerMi = seconds / miles;
 	let hoursResult = secPerMi / 3600;
@@ -19,7 +29,7 @@ function calc() {
 	hoursResult = Math.floor(hoursResult);
 	minsResult =  Math.floor(minsResult);
 	secsResult =  Math.round(secsResult*100)/100;
-	consoleText = "You ran a <b>" + (hoursResult ? hoursResult + " hour " : "") + (minsResult ? minsResult + " min " : "") + (secsResult ? secsResult + " sec" : "") + " </b> mile"
+	consoleText = 'You ran a <b>' + (hoursResult ? hoursResult + ' hour ' : '') + (minsResult ? minsResult + ' min ' : '') + (secsResult ? secsResult + ' sec' : '') + ' </b> mile';
 	$('#result').html(consoleText);
 	$('#consolePara').after(consoleText + "<br>");
 }
@@ -28,5 +38,11 @@ $(function() {
 	$('input').on('click', function() {
 		$(this).select();
 	});
+	$('#calcButton').on('click', calc);
+	
+	$('#hr').val('1');
+	$('#mi').val('1');
+
+
 	$('#hr').select();
 });
